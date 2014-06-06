c898f0e368584bf32ce1d580723c4414ecbb44b4
antidotcb <antidotcb@gmail.com>
2014-06-06 09:09:22 +0300

    add diagrams

--- trucks.cpp
+++ trucks.cpp
@@ -1,5 +1,7 @@
 #include "trucks.h"
 #include <qdebug.h>
+#include <QGraphicsPixmapItem>
+
 Trucks::Trucks(QWidget *parent) : QMainWindow(parent) {
     ui.setupUi(this);
     int i=0;
@@ -10,6 +12,22 @@ Trucks::Trucks(QWidget *parent) : QMainWindow(parent) {
             widget->hide();
         }
     }
+
+
+    QGraphicsPixmapItem* item1 = new QGraphicsPixmapItem(QPixmap(":/images/Resources/table.png"));
+    QGraphicsScene* scene = new QGraphicsScene();
+    ui.graphicsView_3->setScene(scene);
+    scene->addItem(item1);
+
+    QGraphicsPixmapItem* item2= new QGraphicsPixmapItem(QPixmap(":/images/Resources/DispDiagr.png"));
+    QGraphicsScene* scene1 = new QGraphicsScene();
+    ui.graphicsView->setScene(scene1);
+    scene1->addItem(item2);
+
+    QGraphicsPixmapItem* item3 = new QGraphicsPixmapItem(QPixmap(":/images/Resources/Gantt.png"));
+    QGraphicsScene* scene2 = new QGraphicsScene();
+    ui.graphicsView_2->setScene(scene2);
+    scene2->addItem(item3);
 }
 
 Trucks::~Trucks()
